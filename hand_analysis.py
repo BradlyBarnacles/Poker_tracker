b63c6e46afdb166b55b607208c136d97a8431c2e
rats-can-fly <calum.octopus@gmail.com>
2020-06-29 12:16:56 +0100

    Player data now saved as pandas Dataframe

--- hand_analysis.py
+++ hand_analysis.py
@@ -4,76 +4,93 @@ from decimal import Decimal
 import matplotlib.pyplot as plt
 import shutil
 import handhistory as HH
+import pandas as pd
+import numpy as np
 
 in_path = "C:/Users/Calum/AppData/Local/PokerStars.UK/HandHistory/dazzle0"
 out_path = "E:/Poker/hand_data"
+my_user = "dazzle0"
 
 
-def profit():
-    total = [0]
-    for file in os.listdir(path):
-        with open(os.path.join(path, file), "r", encoding='utf-8-sig') as f:
-            hands = ijson.items(f, 'item')
-            chips = Decimal(2.00)
-            for hand in hands:
-                net = hand["players"]['dazzle0']['winnings'] - hand["players"]['dazzle0']['into_pot']
-                if chips != hand["players"]['dazzle0']["chip_stack"]:
-                    print(hand["hand_number"], hand["time"])
-                total.append(total[-1]+ change)
-                chips += change
 
-    plt.plot(total)
-    plt.show()
+def player_winnings(hand, player):
+    net = hand["players"][player]['winnings'] - hand["players"][player]['into_pot']
+    return {"winnings": net}
 
-    print(total[-1])
-        
+def round_seen(hand, player):
+    stats = {"total_hands": 1,
+             "flops_seen": 0}
+    for action in hand["pre_flop_action"]:
+        if action["player"] == player and action["action"] == "folds":
+            break
+    else:
+        stats["flops_seen"] = 1
+    return stats
 
-    
-with open("player_data.txt", "r") as f:
-    player_data = json.loads(f.read())
-with open("earnings.txt", "r") as f:
-    earnings = json.loads(f.read())
-    
+## analytics is a list of functions, each of which return 1 or more statistics about a given player
+analytics = [round_seen, player_winnings]
+
+
+##updates the player data dictionary accoring to the stats returned by and analytic
+def update(player_data, new_stats):
+    for stat_name, val in new_stats.items():
+        try:
+            player_data[stat_name] += val
+        except:
+            player_data[stat_name] = val
+    return
+
+
+
+## creates a pandas data_frame with existing player data if it exists, otherwise creates an empty data frame.
+try:
+    player_data = pd.read_csv("player_data.csv")
+except:
+    player_data = pd.DataFrame(columns = ["session", "player", "total_hands", "flops_seen", "winnings"])
+    player_data.set_index('player')
+
+
+try:
+    with open("earnings.txt", "r") as f:
+        earnings = json.loads(f.read())
+except:
+    earnings = {}
+
 for file in os.listdir(in_path):
+    
     f1 = os.path.join(in_path, file)
     f2 = os.path.join(out_path, file)
-    if not ( os.path.exists(f2) ) or os.path.getmtime(f1) > os.path.getmtime(f2):
-        player_data[f2] = {}
+    
+    if True: ## not ( os.path.exists(f2) ) or os.path.getmtime(f1) > os.path.getmtime(f2):           ##if file is new or updated
+        
+        player_data = player_data[player_data.session != f2] ## remove pre-existing entries relating to this session
+        new_data = {}
+        #new dict for stats data from this session
         earnings[f2] = [0]
         shutil.copy(f1, f2)
         for hand in HH.Parse_hands(f2):
-            for player in hand["players"]:
-                if player in player_data[f2].keys():
-                    player_data[f2][player]["hands"] += 1
-                else:
-                    player_data[f2][player] = {"hands": 1, "flops_seen": 0, "winnings": 0}
-                    
-                net = hand["players"][player]['winnings'] - hand["players"][player]['into_pot']
-                if player == "dazzle0":
+            for analytic in analytics:
+                
+                for player in hand["players"]:
+                    if player not in new_data.keys():
+                        new_data[player] = {"session": f2, "player": player}
+    
+                    update(new_data[player], analytic(hand,player))
+                        
+                    net = hand["players"][my_user]['winnings'] - hand["players"][my_user]['into_pot']
                     earnings[f2].append(net)
-                player_data[f2][player]["winnings"] += net
-                    
-                for action in hand["pre_flop_action"]:
-                    if action["player"] == player and action["action"] == "folds":
-                        break
-                else:
-                    player_data[f2][player]["flops_seen"] += 1
+
+        player_data = player_data.append(pd.DataFrame(new_data.values()), ignore_index = True)
+
+
 
     
 
-with open("player_data.txt", "w") as f:
-    f.write(json.dumps(player_data))
+player_data.to_csv("player_data.csv")
 with open("earnings.txt", "w") as f:
     f.write(json.dumps(earnings))
 
-totals = {}
-for file, session in player_data.items():
-    for player, stats in session.items():
-        try:
-            for statistic, val in stats.items():
-                totals[player][statistic] += val
-        except:
-            totals[player] = stats
+
 
 cum_earnings = [0]
 for session in earnings.values():
